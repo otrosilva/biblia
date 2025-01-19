49a35366482a3e6a8479c47e3d68a5ad2079453e
otrosilva <otrosilva@icloud.com>
2025-01-19 21:00:16 +0300

    Opción random y mejora en la salida

---
+++ biblia.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+"""Lector de la Biblia en formato Zefania XML.
+
+Este script permite leer y mostrar información de la Biblia a partir de un archivo en formato Zefania XML.
+Puede listar todos los libros y capítulos, mostrar un libro completo, un capítulo específico
+o un versículo en particular.
+
+El archivo Zefania XML debe estar ubicado en el directorio de descargas del usuario (~Downloads/biblia.xml).
+
+Uso:
+    ./biblia.py
+    ./biblia.py [libro]
+    ./biblia.py [libro] [capítulo]
+    ./biblia.py [libro] [capítulo] [versículo]
+    ./biblia.py -s "texto a buscar"
+"""
+import os
+import xml.etree.ElementTree as ET
+import sys
+import difflib
+import tempfile
+
+
+class BibleReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_xml_file(self):
+        try:
+            tree = ET.parse(self.file_path)
+            self.root = tree.getroot()
+        except Exception as e:
+            print(
+                f"Error: No se pudo abrir o analizar el archivo: {self.file_path}. {e}"
+            )
+            sys.exit(1)
+
+    def list_books(self):
+        output = []
+        output.append("Libros y Capítulos:")
+        books = []
+        for book in self.root.findall(".//BIBLEBOOK"):
+            book_name = book.get("bname")
+            chapters = book.findall(".//CHAPTER")
+            books.append(book_name.lower())
+            output.append(f"{book_name} [{len(chapters)}]")
+        return "\n".join(output)
+
+    def suggest_similar_books(self, input_book):
+        books = [
+            book.lower()
+            for book in [
+                book.get("bname") for book in self.root.findall(".//BIBLEBOOK")
+            ]
+        ]
+        suggestions = difflib.get_close_matches(
+            input_book.lower(), books, n=3, cutoff=0.6
+        )
+
+        if suggestions:
+            return f"El libro '{input_book}' no se encuentra.\n¿Quisiste decir: {', '.join(suggestions)}?"
+        else:
+            return f"El libro '{input_book}' no se encuentra. No se encontraron libros similares."
+
+    def show_book(self, book_name):
+        found = False
+        output = []
+        books = [
+            book.get("bname").lower() for book in self.root.findall(".//BIBLEBOOK")
+        ]
+        if book_name.lower() in books:
+            found = True
+            for book in self.root.findall(".//BIBLEBOOK"):
+                if book.get("bname").lower() == book_name.lower():
+                    for chapter in book.findall(".//CHAPTER"):
+                        chapter_number = chapter.get("cnumber")
+                        for verse in chapter.findall(".//VERS"):
+                            verse_number = verse.get("vnumber")
+                            output.append(
+                                f"[{book.get('bname')} {chapter_number}:{verse_number}] \n{verse.text}"
+                            )
+        if not found:
+            output.append(self.suggest_similar_books(book_name))
+        return "\n".join(output)
+
+    def show_chapter(self, book_name, chapter_number):
+        found_book = False
+        found_chapter = False
+        output = []
+        for book in self.root.findall(".//BIBLEBOOK"):
+            if book.get("bname").lower() == book_name.lower():
+                found_book = True
+                for chapter in book.findall(".//CHAPTER"):
+                    if chapter.get("cnumber") == chapter_number:
+                        found_chapter = True
+                        for verse in chapter.findall(".//VERS"):
+                            verse_number = verse.get("vnumber")
+                            output.append(
+                                f"[{book.get('bname')} {chapter_number}:{verse_number}] \n{verse.text}"
+                            )
+                        break
+                break
+
+        if not found_book:
+            output.append(self.suggest_similar_books(book_name))
+        elif not found_chapter:
+            output.append(
+                f"El capítulo '{chapter_number}' no se encuentra en el libro '{book_name}'."
+            )
+        return "\n".join(output)
+
+    def show_verse(self, book_name, chapter_number, verse_number):
+        found_book = False
+        found_chapter = False
+        found_verse = False
+        output = []
+        for book in self.root.findall(".//BIBLEBOOK"):
+            if book.get("bname").lower() == book_name.lower():
+                found_book = True
+                for chapter in book.findall(".//CHAPTER"):
+                    if chapter.get("cnumber") == chapter_number:
+                        found_chapter = True
+                        for verse in chapter.findall(".//VERS"):
+                            if verse.get("vnumber") == verse_number:
+                                found_verse = True
+                                output.append(
+                                    f"[{book.get('bname')} {chapter_number}:{verse_number}] \n{verse.text}"
+                                )
+                                break
+                        break
+
+        if not found_book:
+            output.append(f"El libro '{book_name}' no se encuentra.")
+            output.append(self.suggest_similar_books(book_name))
+        elif not found_chapter:
+            output.append(
+                f"El capítulo '{chapter_number}' no se encuentra en el libro '{book_name}'."
+            )
+        elif not found_verse:
+            output.append(
+                f"El versículo '{verse_number}' no se encuentra en el capítulo '{chapter_number}' del libro '{book_name}'."
+            )
+        return "\n".join(output)
+
+    def search_text(self, search_text):
+        output = []
+        for book in self.root.findall(".//BIBLEBOOK"):
+            book_name = book.get("bname")
+            for chapter in book.findall(".//CHAPTER"):
+                chapter_number = chapter.get("cnumber")
+                for verse in chapter.findall(".//VERS"):
+                    verse_number = verse.get("vnumber")
+                    verse_text = verse.text
+                    if search_text.lower() in verse_text.lower():
+                        output.append(
+                            f"[{book_name} {chapter_number}:{verse_number}] \n{verse_text}"
+                        )
+        if not output:
+            return (
+                f"No se encontraron versículos que contengan el texto '{search_text}'."
+            )
+        return "\n".join(output)
+
+
+def main():
+    file_path = os.path.expanduser("~/Downloads/biblia.xml")
+    bible_reader = BibleReader(file_path)
+    bible_reader.load_xml_file()
+
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        output = bible_reader.list_books()
+    elif len(args) == 1:
+        output = bible_reader.show_book(args[0])
+    elif len(args) == 2 and args[0] == "-s":
+        output = bible_reader.search_text(args[1])
+    elif len(args) == 2:
+        output = bible_reader.show_chapter(args[0], args[1])
+    elif len(args) == 3:
+        output = bible_reader.show_verse(args[0], args[1], args[2])
+    else:
+        output = "Error: Número de argumentos no válido."
+
+    # Escribir salida en un archivo temporal y luego mostrarlo con cat
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+        temp_file.write(output)
+        temp_file_path = temp_file.name
+
+    os.system(f"cat {temp_file_path}")
+    os.remove(temp_file_path)
+
+
+if __name__ == "__main__":
+    main()
